3a26a2b6e58c09ba528d4d19c2894d453d940ee9
ManigandanM-BITS <2022AC05144@wilp.bits-pilani.ac.in>
2024-07-29 22:41:49 +0530

    Fix trailing whitespace and missing final newline4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 """
 This is a simple script to demonstrate MLOps.
 """
@@ -7,6 +8,6 @@ def main():
     Main function to print a message.
     """
     print("Hello, MLOps Group 38!")
- 
+
 if __name__ == "__main__":
     main()
