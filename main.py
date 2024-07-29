b24a872bf3bf3353b79649e7a8ac8fb16aa278a6
ManigandanM-BITS <2022AC05144@wilp.bits-pilani.ac.in>
2024-07-29 22:25:28 +0530

    Fix pylint issues in main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+"""
+This is a simple script to demonstrate MLOps.
+"""
+
 def main():
+    """
+    Main function to print a message.
+    """
     print("Hello, MLOps Group 38!")
     
 if __name__ == "__main__":
